269c659ddc079ee4f0943978c8d046378bec38db
Nikolas Schuetz <n.schuetz@hotmail.de>
2025-03-22 14:04:53 +0100

    Added an input eventhandler should dsiplay the keyboard events on the Monitor

--- uefi/main.zig
+++ uefi/main.zig
@@ -7,15 +7,36 @@ pub fn main() uefi.Status {
     const sys_table = uefi.system_table;
     // conout is an optional so therfore we have to make sure that the null case is handled properly
     const conout = sys_table.con_out orelse return uefi.Status.load_error;
+    const conin = sys_table.con_in orelse return uefi.Status.load_error;
+    const boot_services = sys_table.boot_services orelse return uefi.Status.load_error;
 
     var Status = uefi.Status.success;
+    var index: usize = undefined;
+    var input_key: uefi.protocol.SimpleTextInputEx.Key.Input = undefined;
 
     Status = conout.clearScreen();
     if (Status != uefi.Status.success) {
         return Status;
     }
+
     // _ = is used to ignore the return value as Zig does not accept untreated return values
     _ = conout.outputString(utf16("Hello World ! \n"));
+
+    const input_events = [_]uefi.Event{
+        conin.wait_for_key,
+    };
+
+    while (boot_services.waitForEvent(input_events.len, &input_events, &index) == uefi.Status.success) {
+        if (index == 0) {
+            if (conin.readKeyStroke(&input_key) == uefi.Status.success) {
+                if (input_key.unicode_char == @as(u16, 'Q')) {
+                    return uefi.Status.success;
+                }
+                const slice: [*:0]const u16 = &[_:0]u16{input_key.unicode_char};
+                _ = conout.outputString(slice);
+            }
+        }
+    }
 
     return uefi.Status.success;
 }
